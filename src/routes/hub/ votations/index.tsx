dfcfdd58dc43d0ffdfab532c59f7406e9afce14b
nick <dev@labict.it>
2023-09-05 20:40:06 -0300

    Corrections visual

--- src/routes/hub/ votations/index.tsx
+++ src/routes/hub/ votations/index.tsx
@@ -5,6 +5,7 @@ import { useAppSelector } from "../../../hooks/store/store";
 import { useEffect } from "react";
 import IonIcon from "@reacticons/ionicons";
 import GetDemo from "../../resources/getdemo";
+import UserNavbar from "../components/panelnombreuser";
 
 const VotationsHub = () => {
     const {user} = useAppSelector(getStore);
@@ -18,34 +19,38 @@ const VotationsHub = () => {
     return (
     <div className="displayFlex">
         <NavbarHub/>
-        <div className="hub-container w100" style={{flexBasis: '100%', padding: '2rem'}}>
-            <div className="displayFlex between w100">
-                <h1 style={{fontSize: '3rem', marginTop: 'auto'}}>Your votations</h1>
+        
+        <div className="hub-container w100" >
+            <UserNavbar/>
+            <div style={{flexBasis: '100%', padding: '2rem'}}>
+                <div className="displayFlex between w100">
+                    <h1 style={{fontSize: '3rem', marginTop: 'auto'}}>Your votations</h1>
 
-                <div >
-                    <button onClick={() => navigate('/create')} className="btn btn-primary" style={{marginTop: '0rem'}}><IonIcon name="add-outline" /> Create</button>
+                    <div >
+                        <button onClick={() => navigate('/create')} className="btn btn-primary" style={{marginTop: '0rem'}}><IonIcon name="add-outline" /> Create</button>
+                    </div>
                 </div>
-            </div>
-            <div className="displayFlex between w100">
-                <input  placeholder="Search"></input>
-                <input style={{marginLeft: '.5rem'}} type="date"  placeholder="Search"></input>
+                <div className="displayFlex between w100">
+                    <input  placeholder="Search"></input>
+                    <input style={{marginLeft: '.5rem'}} type="date"  placeholder="Search"></input>
 
-                
-            </div>
-            <div className="table-white-background">
-                <h1 style={{fontSize: '1.5rem'}}> Eleccionas Primarias</h1>
-                
-                <IonIcon name="chevron-forward-outline" style={{fontSize: '1.5rem', color: 'grey', cursor: 'pointer' ,position: 'absolute', top: '2rem', right: '1rem'}}/>
-            </div>
-            <div className="table-white-background">
-                <h1 style={{fontSize: '1.5rem'}}> Paso 2023</h1>
-                <GetDemo/>
-                <IonIcon name="chevron-forward-outline" style={{fontSize: '1.5rem', color: 'grey', cursor: 'pointer' ,position: 'absolute', top: '2rem', right: '1rem'}}/>
-            </div>
-            <div className="table-white-background">
-                <h1 style={{fontSize: '1.5rem'}}> Paso 2023</h1>
-                <GetDemo/>
-                <IonIcon name="chevron-forward-outline" style={{fontSize: '1.5rem', color: 'grey', cursor: 'pointer' ,position: 'absolute', top: '2rem', right: '1rem'}}/>
+                    
+                </div>
+                <div className="table-white-background">
+                    <h1 style={{fontSize: '1.5rem'}}> Eleccionas Primarias</h1>
+                    
+                    <IonIcon name="chevron-forward-outline" style={{fontSize: '1.5rem', color: 'grey', cursor: 'pointer' ,position: 'absolute', top: '2rem', right: '1rem'}}/>
+                </div>
+                <div className="table-white-background">
+                    <h1 style={{fontSize: '1.5rem'}}> Paso 2023</h1>
+                    <GetDemo/>
+                    <IonIcon name="chevron-forward-outline" style={{fontSize: '1.5rem', color: 'grey', cursor: 'pointer' ,position: 'absolute', top: '2rem', right: '1rem'}}/>
+                </div>
+                <div className="table-white-background">
+                    <h1 style={{fontSize: '1.5rem'}}> Paso 2023</h1>
+                    <GetDemo/>
+                    <IonIcon name="chevron-forward-outline" style={{fontSize: '1.5rem', color: 'grey', cursor: 'pointer' ,position: 'absolute', top: '2rem', right: '1rem'}}/>
+                </div>
             </div>
         </div>
     </div>
--- src/routes/hub/components/panelnombreuser.tsx
+++ src/routes/hub/components/panelnombreuser.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import IonIcon from "@reacticons/ionicons";
 const UserNavbar = () => {
   return (
-    <div className='container-cont'>
+    <div className='container-cont displayFlex between'>
+      <div>
+        <IonIcon name="help-outline" className='iconss noti-orange' title='Whath is Pricing?'></IonIcon>
+      </div>
         <section className='hijo-container'>
-        <IonIcon name="notifications-outline" className='iconss noti-orange'></IonIcon>
+          <p>Ferra Alexandra</p>    
             <div className='cicle'>
                 <img src="https://images.unsplash.com/photo-1692035053253-c40149437b5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" alt="" />
             </div>
-          <p>Ferra Alexandra</p>    
-        <IonIcon name="chevron-down-outline" className='iconss'></IonIcon>
+          
           
         </section>
     </div>
