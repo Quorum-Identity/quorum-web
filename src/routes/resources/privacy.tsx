71a792b41eb5ec8d037702b76354a9adec8ae078
Ramiro <ramisilva8@gmail.com>
2023-10-17 12:12:34 -0300

    traslate2-complete

--- src/routes/resources/privacy.tsx
+++ src/routes/resources/privacy.tsx
@@ -8,23 +8,21 @@ const PrivacyPolicy = () => {
     return <>
         <Navbar t = {t} i18n={i18n}/>
         <div className="container mt2">
-            <h1 >Privacy Policy</h1>
-            <h2 style={{fontSize: '2rem'}}>1. Information Collection</h2>
-            <p>When you use our application, we may collect certain personal data necessary for the KYC process, such as your name, address, date of birth, identification number, and a photograph of your official identification document. This information is essential to verify your identity and ensure that only you can access our services.</p>
-            <h2 style={{fontSize: '2rem'}}>2. Use of Information</h2>
-            <p>The information collected during the KYC process will be used solely for the purpose of verifying your identity and ensuring the integrity and security of our operations. We will not share or sell your personal data to third parties for marketing or advertising purposes.</p>
-            <h2 style={{fontSize: '2rem'}}>3. Data Security</h2>
-            <p>We take rigorous measures to protect your personal data against unauthorized access, disclosure, or destruction. We use technical and administrative security measures to ensure that your information is secure and confidential.</p>
-            <h2 style={{fontSize: '2rem'}}>4. Third-Party Access</h2>
-            <p>To carry out the KYC process, we may share your data with external entities that provide verification and compliance services. However, we ensure that these entities comply with strict data security and privacy standards.</p>
-            <h2 style={{fontSize: '2rem'}}>5. Data Retention</h2>
-            <p>We will retain your personal data for as long as necessary to comply with our legal and regulatory obligations. Once it is no longer needed, we will delete or anonymize the information to protect your privacy.</p>
-            <h2 style={{fontSize: '2rem'}}>6. User Rights</h2>
-            <p>You have the right to access, correct, or delete your personal data that we have stored. If you wish to exercise these rights or have any questions about our Privacy Policy, you can contact us through sopport@quorum.com</p>
-            <h2 style={{fontSize: '2rem'}}>7. User Rights</h2>
-            <p>We reserve the right to modify this Privacy Policy at any time to reflect changes in our privacy practices. We recommend that you review it periodically to stay informed about how we protect your information.
-
-                By using our application, you agree to the terms and conditions of this Privacy Policy. If you do not agree with these terms, we ask that you do not use our services.
+            <h1 >{t("51345.p1")}</h1>
+            <h2 style={{fontSize: '2rem'}}>{t("31341.p1")}</h2>
+            <p>{t("9801.p1")}</p>
+            <h2 style={{fontSize: '2rem'}}>{t("1231.p1")}</h2>
+            <p>{t("8781.p1")}</p>
+            <h2 style={{fontSize: '2rem'}}>{t("9810.p1")}</h2>
+            <p>{t("9865.p1")}</p>
+            <h2 style={{fontSize: '2rem'}}>{t("13566.p1")}</h2>
+            <p>{t("58161.p1")}</p>
+            <h2 style={{fontSize: '2rem'}}>{t("19286.p1")}</h2>
+            <p>{t("123851.p1")}</p>
+            <h2 style={{fontSize: '2rem'}}>{t("131111.p1")}</h2>
+            <p>{t("123125.p1")}</p>
+            <h2 style={{fontSize: '2rem'}}>{t("12347.p1")}</h2>
+            <p> {t("56144.p1")}
                 </p>
             </div>
         <FooterApp t = {t} i18n={i18n}/>
--- src/routes/resources/terms.tsx
+++ src/routes/resources/terms.tsx
@@ -8,23 +8,15 @@ const TermsConditions = () => {
     return <>
         <Navbar t = {t} i18n={i18n}/>
         <div className="container mt2">
-            <h1 >Términos y Condiciones</h1>
-            <h2 style={{fontSize: '2rem'}}>1. Privacidad de Datos</h2>
-            <p>1.1. Compromiso de Privacidad
-Nos tomamos muy en serio la privacidad de tus datos. Garantizamos que tus datos personales no serán compartidos, vendidos ni divulgados a terceros sin tu consentimiento expreso, excepto en las circunstancias que se describen en nuestra Política de Privacidad.
-
-1.2. Protección de Datos
-Implementamos medidas de seguridad y protección de datos para garantizar la confidencialidad y la integridad de la información que nos proporcionas.
-</p>
-            <h2 style={{fontSize: '2rem'}}>2. Recopilación de Datos</h2>
-            <p>2.1. Información del Usuario
-Para utilizar nuestra plataforma, recopilaremos cierta información personal necesaria para brindarte nuestros servicios. Esta información se utiliza únicamente para los fines específicos de la plataforma y de acuerdo con nuestra Política de Privacidad.</p>
-            <h2 style={{fontSize: '2rem'}}>3. Responsabilidades de los Usuarios</h2>
-            <p>3.1. Uso Adecuado
-Te comprometes a utilizar la plataforma de acuerdo con las leyes y regulaciones aplicables, así como con estos Términos y Condiciones. No debes utilizar la plataforma con fines ilegales o inapropiados.</p>
-            <h2 style={{fontSize: '2rem'}}>4. Cambios en los Términos y Condiciones</h2>
-            <p>4.1. Actualizaciones
-Nos reservamos el derecho de modificar estos Términos y Condiciones en cualquier momento. Cualquier cambio se publicará en la plataforma y entrará en vigencia inmediatamente. Te recomendamos revisar estos términos periódicamente.</p>
+            <h1 >{t("5131.p1")}</h1>
+            <h2 style={{fontSize: '2rem'}}>{t("1257.p1")}</h2>
+            <p>{t("12356.p1")}</p>
+            <h2 style={{fontSize: '2rem'}}>{t("56743.p1")}</h2>
+            <p>{t("214156.p1")}</p>
+            <h2 style={{fontSize: '2rem'}}>{t("5144.p1")}</h2>
+            <p>{t("11116.p1")}</p>
+            <h2 style={{fontSize: '2rem'}}>{t("5672.p1")}</h2>
+            <p>{t("4556.p1")}</p>
             
             </div>
         <FooterApp t = {t} i18n={i18n}/>
